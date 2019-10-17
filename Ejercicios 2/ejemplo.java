c49833e8c464cabd7b8e45ceb927a23d4fec73ea
hernanSan <hasanchez9qutpl.edu.ec>
2019-10-17 12:58:46 -0500

    ejerciciosdeJAVA

---
+++ Ejercicios 2/ejemplo.java
@@ -0,0 +1,24 @@
+ejemplo.java
+" No se instalo java"
+public classsjemplo extends Parent implements Interface {
+	public static void main(string)[] args) {int edad;
+	edad=30+2-1;
+	String nombres;
+nombres = "Hernan Alejandro";
+	String apellidos;
+	apellidos= "Sanchez Matamoros";
+	String edad;
+	edad= "18";
+	String ciudad naciemto;
+	ciudad de naciemto= "Loja";
+	String universidad;
+	universidad= "Universiad Nacional de Loja";
+
+	System.out.println(nombre);
+	System.out.println(apellidos);
+	System.out.println(edad);
+	System.out.println(ciudad de naciemto);
+
+
+	
+}
